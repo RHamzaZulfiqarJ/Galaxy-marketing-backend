50f0d07dddf4953b2fa97c0359de7b95b7a5dc44
Glitch (glitch-hello-node) <none>
2024-11-06 11:41:41 +0000

    🛀🕴 Checkpoint ./server.js:61060117/6058

--- server.js
+++ server.js
@@ -1,119 +1,73 @@
-/**
- * This is the main Node.js server script for your project
- * Check out the two endpoints this back-end API provides in fastify.get and fastify.post below
- */
+import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-const path = require("path");
+import uploadRoutes from './routes/upload.js'
+import authRoutes from './routes/auth.js'
+import userRoutes from './routes/user.js'
+import notificationRoutes from './routes/notification.js'
+import saleRoutes from './routes/sale.js'
+import taskRoutes from './routes/task.js'
+import eventRoutes from './routes/event.js'
+import approvalRoutes from './routes/approval.js'
+import leadRoutes from './routes/lead.js'
+import followUpRoutes from './routes/followUp.js'
+import cashbookRoutes from './routes/cashbook.js'
+import refundRoutes from './routes/refund.js'
+import voucherRoutes from './routes/voucher.js'
+import deductionRoutes from './routes/deduction.js'
+import transcriptRoutes from './routes/transcript.js'
+import projectRoutes from './routes/project.js'
+import societyRoutes from './routes/society.js'
+import inventoryRoutes from './routes/inventory.js'
 
-// Require the fastify framework and instantiate it
-const fastify = require("fastify")({
-  // Set this to true for detailed logging:
-  logger: false,
-});
+dotenv.config()
+const app = express()
+const CONNECTION_URL = process.env.ATLAS_URL
+// const CONNECTION_URL = process.env.COMPASS_URL
 
-// ADD FAVORITES ARRAY VARIABLE FROM TODO HERE
+const PORT = process.env.PORT || 5000
+app.use(cors())
+app.use(express.json())
 
-// Setup our static files
-fastify.register(require("@fastify/static"), {
-  root: path.join(__dirname, "public"),
-  prefix: "/", // optional: default '/'
-});
+// serving static files | images
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+app.use('/uploads', express.static(join(__dirname, 'uploads')));
 
-// Formbody lets us parse incoming forms
-fastify.register(require("@fastify/formbody"));
+app.get('/', (req, res) => {
+    res.send('Hello to API')
+})
 
-// View is a templating manager for fastify
-fastify.register(require("@fastify/view"), {
-  engine: {
-    handlebars: require("handlebars"),
-  },
-});
+app.use('/api/v1', uploadRoutes)
+app.use('/api/v1/auth', authRoutes)
+app.use('/api/v1/user', userRoutes)
+app.use('/api/v1/notification', notificationRoutes)
+app.use('/api/v1/task', taskRoutes)
+app.use('/api/v1/event', eventRoutes)
+app.use('/api/v1/approval', approvalRoutes)
+app.use('/api/v1/sale', saleRoutes)
+app.use('/api/v1/lead', leadRoutes)
+app.use('/api/v1/project', projectRoutes)
+app.use('/api/v1/society', societyRoutes)
+app.use('/api/v1/inventory', inventoryRoutes)
+app.use('/api/v1/followUp', followUpRoutes)
+app.use('/api/v1/cashbook', cashbookRoutes)
+app.use('/api/v1/refund', refundRoutes)
+app.use('/api/v1/voucher', voucherRoutes)
+app.use('/api/v1/deduction', deductionRoutes)
+app.use('/api/v1/trasncript', transcriptRoutes)
+ 
+app.use((err, req, res, next) => {
+    const message = err.message || 'Something went wrong.'
+    const status = err.status || 500
+    res.status(status).json({ message, status, stack: err.stack })
+    next()
+})
 
-// Load and parse SEO data
-const seo = require("./src/seo.json");
-if (seo.url === "glitch-default") {
-  seo.url = `https://${process.env.PROJECT_DOMAIN}.glitch.me`;
-}
-
-/**
- * Our home page route
- *
- * Returns src/pages/index.hbs with data built into it
- */
-fastify.get("/", function (request, reply) {
-  // params is an object we'll pass to our handlebars template
-  let params = { seo: seo };
-
-  // If someone clicked the option for a random color it'll be passed in the querystring
-  if (request.query.randomize) {
-    // We need to load our color data file, pick one at random, and add it to the params
-    const colors = require("./src/colors.json");
-    const allColors = Object.keys(colors);
-    let currentColor = allColors[(allColors.length * Math.random()) << 0];
-
-    // Add the color properties to the params object
-    params = {
-      color: colors[currentColor],
-      colorError: null,
-      seo: seo,
-    };
-  }
-
-  // The Handlebars code will be able to access the parameter values and build them into the page
-  return reply.view("/src/pages/index.hbs", params);
-});
-
-/**
- * Our POST route to handle and react to form submissions
- *
- * Accepts body data indicating the user choice
- */
-fastify.post("/", function (request, reply) {
-  // Build the params object to pass to the template
-  let params = { seo: seo };
-
-  // If the user submitted a color through the form it'll be passed here in the request body
-  let color = request.body.color;
-
-  // If it's not empty, let's try to find the color
-  if (color) {
-    // ADD CODE FROM TODO HERE TO SAVE SUBMITTED FAVORITES
-
-    // Load our color data file
-    const colors = require("./src/colors.json");
-
-    // Take our form submission, remove whitespace, and convert to lowercase
-    color = color.toLowerCase().replace(/\s/g, "");
-
-    // Now we see if that color is a key in our colors object
-    if (colors[color]) {
-      // Found one!
-      params = {
-        color: colors[color],
-        colorError: null,
-        seo: seo,
-      };
-    } else {
-      // No luck! Return the user value as the error property
-      params = {
-        colorError: request.body.color,
-        seo: seo,
-      };
-    }
-  }
-
-  // The Handlebars template will use the parameter values to update the page with the chosen color
-  return reply.view("/src/pages/index.hbs", params);
-});
-
-// Run the server and report out to the logs
-fastify.listen(
-  { port: process.env.PORT, host: "0.0.0.0" },
-  function (err, address) {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-    console.log(`Your app is listening on ${address}`);
-  }
-);
+mongoose.connect(CONNECTION_URL)
+    .then(() => app.listen(PORT, () => console.log('listening at port ' + PORT)))
+    .catch((err) => console.log('error in connection with mongoDB = \n', err))
