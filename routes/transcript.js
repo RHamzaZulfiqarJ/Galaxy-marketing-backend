4b3f0f25b56ea3fc0aae89dcd19ab14b8da34713
Glitch (glitch-hello-node) <none>
2024-11-06 14:48:54 +0000

    🏆🌺 Checkpoint ./routes/transcript.js:61060117/2066

--- routes/transcript.js
+++ routes/transcript.js
@@ -1,36 +1,20 @@
-import express from 'express'
-import { createTask, getTask, getUserTasks, updateTask, deleteTask, deleteWholeCollection, } from '../controllers/task.js'
-import { verifyEmployee, verifyToken } from '../middleware/auth.js'
-import Task from '../models/task.js'
-import { createError } from '../utils/error.js'
+import express from "express";
+import { getTranscript, createtranscript, deleteTranscript, getTranscripts, updateTranscript } from "../controllers/transcript.js";
+import { verifyEmployee, verifyManager, verifySuperAdmin, verifyToken } from '../middleware/auth.js'
 
 const router = express.Router()
 
-const verifyIsSame = async (req, res, next) => {
-    try {
-        const { taskId } = req.params
-        const findedTask = await Task.findById(taskId)
-        if (!Boolean(findedTask)) return next(createError(400, 'task not exist'))
-
-        if (findedTask.userId == req.user._id || req.user.role == ('manager' || 'super_admin')) next()
-        else next(createError(401, "This task isn't belong to you"))
-    } catch (err) {
-        next(createError(500, err.message))
-    }
-}
-
 // GET
-router.get('/get/all', verifyToken, getUserTasks)
-router.get('/get/single/:taskId', verifyToken, verifyEmployee, verifyIsSame, getTask)
+router.get('/get/single/:transcriptId', verifyToken, verifyEmployee, getTranscript, verifyManager)
+router.get('/get/all', verifyToken, verifySuperAdmin, getTranscripts, verifyManager)
 
 // POST
-router.post('/create', verifyToken, verifyEmployee, createTask)
+router.post('/create', verifyToken, verifyManager, verifySuperAdmin, createtranscript)
 
 // PUT
-router.put('/update/:taskId', verifyToken, verifyIsSame, updateTask)
+router.put('/update/:transcriptId', verifyToken, verifyManager, verifySuperAdmin, updateTranscript)
 
 // DELETE
-router.delete('/delete/:taskId', verifyToken, verifyIsSame, deleteTask)
-router.delete('/delete-whole-collection', deleteWholeCollection)
+router.delete('/delete/:transcriptId', deleteTranscript)
 
 export default router
